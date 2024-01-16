ce64010d299cce7f8202475007984a8e26ee2bf0
doriancrutcher <doriankinoocrutcher@gmail.com>
2024-01-15 21:24:16 -0800

    separated app.js into separate files by concept

---
+++ hmac-example.js
@@ -0,0 +1,16 @@
+// HMAC Example 
+
+const {createHmac}=require('crypto')
+
+
+const key = 'super-secret';
+const message = 'boo';
+
+const hmac=createHmac('sha256',key).update(message).digest('hex')
+console.log(hmac)
+
+const key2 = 'other-secret';
+
+
+const hmac2=createHmac('sha256',key2).update(message).digest('hex')
+console.log(hmac2)
